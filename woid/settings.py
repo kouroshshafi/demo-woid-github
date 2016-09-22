9a0ff57b41df35d6eded7db746328000fdaa94fa
kourosh <kouroshshafizadeh@gmail.com>
2016-09-22 21:28:19 +0200

    make it better

--- woid/settings.py
+++ woid/settings.py
@@ -96,15 +96,21 @@ USE_TZ = True
 
 
 #STATIC_ROOT = PROJECT_DIR.parent.parent.child('static')
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = (PROJECT_DIR.child('static'),)
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT,'static/'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#STATICFILES_DIRS = (PROJECT_DIR.child('static'),)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
